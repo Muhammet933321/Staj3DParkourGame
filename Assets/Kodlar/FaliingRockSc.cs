52f72361d33c84ba4bf41c693234259bd230fc97
Muhammet963321 <78547346+Muhammet933321@users.noreply.github.com>
2024-07-29 15:00:54 +0300

    Second Commid

--- Assets/Kodlar/FaliingRockSc.cs
+++ Assets/Kodlar/FaliingRockSc.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class FaliingRockSc : MonoBehaviour
 {
-    [SerializeField] GameObject ParticleEffect;
+    [SerializeField] GameObject RockParticle;
+    [SerializeField] GameObject BloodLake;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Player")
         {
-            Instantiate(ParticleEffect, transform.position, Quaternion.identity);
+            Instantiate(RockParticle, transform.position, Quaternion.identity);
             Destroy(gameObject);
-            
+
+        }
+        else if (collision.gameObject.tag == "Player")
+        {
+            Instantiate(BloodLake, collision.contacts[0].point, Quaternion.identity, collision.gameObject.transform);
         }
     }
 }
